d727faadf40153a9a9ef01de852546d4c8ffd5a5
Zicka67 <art.book@hotmail.fr>
2024-01-09 00:27:25 +0100

    Mise en place du composant react pour le header et home

--- assets/app.ts
+++ assets/app.ts
@@ -2,4 +2,4 @@ import { registerReactControllerComponents } from '@symfony/ux-react';
 import './bootstrap.js';
 import './styles/app.css';
 
-// registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
+registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
