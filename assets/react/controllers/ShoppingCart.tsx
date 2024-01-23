bccde793dcfe6f3ea698cf26a818a7a6ac8b6c9b
Zicka67 <art.book@hotmail.fr>
2024-01-23 01:41:49 +0100

    Creation de la reuqtee AJAX pour createCheckoutSession

--- assets/react/controllers/ShoppingCart.tsx
+++ assets/react/controllers/ShoppingCart.tsx
@@ -3,10 +3,21 @@ import useShoppingCart from "../hooks/useShoppingCart";
 import Header from "./Header";
 import {Box, Button, Container, Grid, Typography} from "@mui/material";
 import ShoppingCartTable from "./ShoppingCartTable";
+import { visit } from "../../utils";
 
 export default function ShoppingCart(){
 
     const {removeItemFromShoppingCart, shoppingCart: ShoppingCart} = useShoppingCart();
+
+    const createCheckoutSession = () => {
+        fetch('/stripe/checkout-sessions', {
+            method: 'POST',
+        })
+        .then(response => response.json())
+        .then(json => {
+            visit(json['url']);
+            });
+    }
 
     return (
         <>
@@ -18,7 +29,7 @@ export default function ShoppingCart(){
                             <Typography variant="h5">Votre panier</Typography>
                         </Grid>
                         <Grid item>
-                            <Button variant="contained" color="primary">
+                            <Button variant="contained" color="primary" onClick={createCheckoutSession}>
                                 Procéder au paiement
                             </Button>
                         </Grid>
