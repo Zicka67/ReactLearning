ec05522345338b32079fb0429965ef25d6f6cd09
Zicka67 <art.book@hotmail.fr>
2024-01-23 23:21:39 +0100

    Function et route du success

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller;
 
-use App\Service\SessionService;
 use App\Service\StripeService;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\SessionService;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StripeController extends AbstractController
 {
@@ -23,6 +24,16 @@ class StripeController extends AbstractController
     {
         return $this->json([
             'url' => $this->stripeService->createCheckoutSession($sessionService->getShoppingCart())->url
+        ]);
+    }
+
+    #[Route('/stripe/success', name: 'success', methods: ['GET'] )]
+    public function success (Request $request): Response
+    {
+        $sessionId = $request->query->getString('session_id');
+
+        return $this->render('stripe/success.html.twig', [
+            "amountTotal" => $this->stripeService->getCheckoutSession($sessionId)->amount_total
         ]);
     }
 }
